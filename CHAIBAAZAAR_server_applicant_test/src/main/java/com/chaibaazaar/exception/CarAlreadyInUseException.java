4ba24829ab3d6a395e16c75b8b0cd067707fbae3
UDAY KIRAN <UDAY KIRAN@LAPTOP-V5J8INTQ>
2021-04-28 10:20:11 +0530

    third commit.

--- CHAIBAAZAAR_server_applicant_test/src/main/java/com/chaibaazaar/exception/CarAlreadyInUseException.java
+++ CHAIBAAZAAR_server_applicant_test/src/main/java/com/chaibaazaar/exception/CarAlreadyInUseException.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Some constraints are violated ...")
 public class CarAlreadyInUseException extends Exception{
-	static final long serialVersionUID = -3387516993224229948L;
+	static final long serialVersionUID = -3387516993434229948L;
 
 
     public CarAlreadyInUseException(String message)
